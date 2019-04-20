54f932b4d62ea9e7925cd4ad8dc3ab1443b761ec
Brian Tsui (tsuio) <35301759+BrianTsui2018@users.noreply.github.com>
2019-04-20 19:42:53 +0800

    Fixed imai-beta: 1) gitignore now doesn't upload credential json (oauth tokens) 2) locally there is a json, 3)index.js added 2 lines to read .env. Remark for Heroku deployment: take out the first 2 lines of index.js. Check tokens in env var.

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ This bot demonstrates many of the core features of Botkit:
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
+var env = require('node-env-file');
+env(__dirname + '/.env');
 
 if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
   usage_tip();
